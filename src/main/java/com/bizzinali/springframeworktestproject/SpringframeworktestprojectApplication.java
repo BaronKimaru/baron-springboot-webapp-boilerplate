d93f896dc235d11800a581bae86ba07a5d0db833
baronkimaru <bizzinali@gmail.com>
2021-10-24 13:57:27 +0300

    first commit

---
+++ src/main/java/com/bizzinali/springframeworktestproject/SpringframeworktestprojectApplication.java
@@ -0,0 +1,13 @@
+package com.bizzinali.springframeworktestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringframeworktestprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringframeworktestprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bizzinali/springframeworktestproject/SpringframeworktestprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizzinali.springframeworktestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframeworktestprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
